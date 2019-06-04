2b6ea5ef3a1d9af264af88d345f0833389170228
valentin <valentin@gmail.com>
2019-06-04 05:35:35 -0500

    Initial commit

---
+++ Assets/Fondo/PlayVideoFondo.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+var movTexture : MovieTexture;
+
+function Update(){
+        movTexture.loop = true;
+        GetComponent.<Renderer>().material.mainTexture = movTexture; // Se indica que la textura se asigna a esa variable
+        movTexture.Play();
+}
